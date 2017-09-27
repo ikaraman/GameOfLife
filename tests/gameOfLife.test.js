66da642b1820deef7df7741b622a4b8bd78bb244
Ivan Karaman <ikaraman@ya.ru>
2017-09-27 18:54:58 +1300

    Unit tests, current progress.

--- tests/gameOfLife.test.js
+++ tests/gameOfLife.test.js
@@ -1,9 +1,17 @@
 "use strict";
 
+const GameOfLife = require("gameOfLife");
+
+console.log = jest.fn(function() {
+});
+
 //source: https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
 describe("Still life tests:", () => {
     it("block should stop execution without ticking", () => {
-
+        let seed = [[0,0,0,0],[0,1,1,0],[0,1,1,0],[0,0,0,0]];
+        
+        GameOfLife.gameOfLife(4, seed);
+        expect(console.log).toHaveBeenLastCalledWith("Execution will stop. New field is the same as a previous one");
     });
 
     it("beehive should stop execution without ticking", () => {
@@ -44,5 +52,19 @@ describe("Oscillator tests:", () => {
 
     });
 
+});
 
+describe("Other tests:", () => {
+    it("empty field should ", () => {
+        let seed = [];
+        GameOfLife.gameOfLife(3, seed);
+
+        expect(console.log).toHaveBeenLastCalledWith("There is no more alive cells");
+    });
+
+    it("calling game of life with size < 2 field should not be allowed", () => {
+        GameOfLife.gameOfLife(1);
+
+        expect(console.log).toHaveBeenLastCalledWith("Field size cannot be less than 2. Execution is stopped.");
+    });
 });
